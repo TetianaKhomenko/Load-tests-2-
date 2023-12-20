f96cf56636a9e66c678610460eaf577c7abdb5e4
Tetiana Khomenko <khomenko.tetiana@gmail.com>
2023-12-20 16:35:48 +0100

    first commit

---
+++ todomvc-tests/todomvc-tests-copy-15/login15_test.js
@@ -0,0 +1,17 @@
+Feature ('Login Page 115');
+
+const email = 'lamin.jaymar@acelap.com';
+const password = 'qwerty'
+
+Scenario ('Login page 115', async ({ I }) => {
+  I.say('I will go to login page now');
+  I.amOnPage('https://beta.testomat.io');
+  I.say('I fulfilled field a password and email');
+  I.fillField('#user_email', email);
+  I.fillField('#user_password', password);
+  I.pressKey('Enter');
+  I.say('I am logged in now');
+  I.waitForElement('#company_id', 30);
+  I.saveScreenshot('login-page.png')
+})
+  
